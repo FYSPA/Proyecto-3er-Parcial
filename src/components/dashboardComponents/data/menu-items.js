7a885f11644580b51e970d8db39fdecda9ce563c
FYSPA <suarezfloresfy@ag.cbtis225.edu.mx>
2025-11-18 22:22:58 -0600

    Arregle el Routing Dinamic, en el Sidebar con un css especifico, por ahora solo con icon de svg de .astro

---
+++ src/components/dashboardComponents/data/menu-items.js
@@ -0,0 +1,28 @@
+// src/data/menu-items.js
+export const menuItems = [
+  {
+    "titulo": "Inicio",
+    "descripcion": "Página principal",
+    "descripcion_icono": "Icono mundo",
+    "texto_etiqueta": "Inicio",
+    "ruta_dinamica": "dashboardpage/Dashboard",
+    "icon": "house"
+  },
+  {
+    "titulo": "Dashboard",
+    "descripcion": "Panel de usuario",
+    "descripcion_icono": "Icono dashboard",
+    "texto_etiqueta": "Panel",
+    "ruta_dinamica": "/login",
+    "icon": ""
+
+  },
+  {
+    "titulo": "Login",
+    "descripcion": "Iniciar Sesión",
+    "descripcion_icono": "Icono login",
+    "texto_etiqueta": "Salir",
+    "ruta_dinamica": "/logout",
+    "icon": ""
+  }
+];
