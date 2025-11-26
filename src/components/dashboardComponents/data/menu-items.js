632c27090d0a43dd35a37054787163658c2b565d
FYSPA <suarezfloresfy@ag.cbtis225.edu.mx>
2025-11-25 22:21:58 -0600

    Cambios de Assets en las imagenes en la MainPage, colocando los juegon en la Dashboard Page. Subiendo todas las imagenes necesarias

--- src/components/dashboardComponents/data/menu-items.js
+++ src/components/dashboardComponents/data/menu-items.js
@@ -11,23 +11,43 @@ export const menuItems = [
     ruta_dinamica: "dashboardpage/Dashboard" //Se especifica la ruta dinamica
   },
   {
-    titulo: "Juego", //Se especifica el titulo
-    descripcion: "Panel de usuario", //Se especifica la descripcion
+    titulo: "Crucible Lament", //Se especifica el titulo
+    descripcion: "GameCrucibleLament", //Se especifica la descripcion
     tipoIcono: "image", //Se especifica el tipo si es 'component' o 'image'
     iconKey: null, //Se coloca null si es image ya que no hay component
-    iconSrc: "/gamesImg/CrucibleLamentIcon.jpg", //Se especifica la ruta de la imagen
-    descripcion_icono: "Icono dashboard", //Se especifica la descripcion del icono
-    texto_etiqueta: "Juego", //Se especifica el texto de la etiqueta
-    ruta_dinamica: "dashboardpage/TheCrucibleLaments" //Se especifica la ruta dinamica
+    iconSrc: "/gamesImg/CrucibleLamentIcon.png", //Se especifica la ruta de la imagen
+    descripcion_icono: "Icono Crucible Lament", //Se especifica la descripcion del icono
+    texto_etiqueta: "Crucible Lament", //Se especifica el texto de la etiqueta
+    ruta_dinamica: "dashboardpage/TheCrucibleLament" //Se especifica la ruta dinamica
   },
   {
-    titulo: "Hola", //Se especifica el titulo
-    descripcion: "Panel de usuario", //Se especifica la descripcion
+    titulo: "Echos of Contention", //Se especifica el titulo
+    descripcion: "GameEchosOfContention", //Se especifica la descripcion
     tipoIcono: "image", //Se especifica el tipo si es 'component' o 'image'
     iconKey: null, //Se coloca null si es image ya que no hay component
-    iconSrc: "/icons/minecraft.svg", //Se especifica la ruta de la imagen
-    descripcion_icono: "Icono dashboard", //Se especifica la descripcion del icono
-    texto_etiqueta: "Hola", //Se especifica el texto de la etiqueta
-    ruta_dinamica: "dashboardpage/" //Se especifica la ruta dinamica
+    iconSrc: "/gamesImg/EchosOfContentionIcon.png", //Se especifica la ruta de la imagen
+    descripcion_icono: "Icono Echos of Contention", //Se especifica la descripcion del icono
+    texto_etiqueta: "Echos of Contention", //Se especifica el texto de la etiqueta
+    ruta_dinamica: "dashboardpage/EchosOfContention" //Se especifica la ruta dinamica
+  },
+  {
+    titulo: "Visionless", //Se especifica el titulo
+    descripcion: "GameVisionless", //Se especifica la descripcion
+    tipoIcono: "image", //Se especifica el tipo si es 'component' o 'image'
+    iconKey: null, //Se coloca null si es image ya que no hay component
+    iconSrc: "/gamesImg/VisionlessIcon.svg", //Se especifica la ruta de la imagen
+    descripcion_icono: "Icono Visionless", //Se especifica la descripcion del icono
+    texto_etiqueta: "Visionless", //Se especifica el texto de la etiqueta
+    ruta_dinamica: "dashboardpage/Visionless" //Se especifica la ruta dinamica
+  },
+  {
+    titulo: "Lost In Space", //Se especifica el titulo
+    descripcion: "GameLostInSpace", //Se especifica la descripcion
+    tipoIcono: "image", //Se especifica el tipo si es 'component' o 'image'
+    iconKey: null, //Se coloca null si es image ya que no hay component
+    iconSrc: "/gamesImg/LostInSpaceIcon.svg", //Se especifica la ruta de la imagen
+    descripcion_icono: "Icono Lost In Space", //Se especifica la descripcion del icono
+    texto_etiqueta: "Lost In Space", //Se especifica el texto de la etiqueta
+    ruta_dinamica: "dashboardpage/LostInSpace" //Se especifica la ruta dinamica
   },
 ];
